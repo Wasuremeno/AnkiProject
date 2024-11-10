68cd2754791579bad9125ba28d4095dc564740f3
Wasuremeno <wasuremono19@gmail.com>
2024-11-11 00:37:05 +0300

    first commit

---
+++ AnkiProject/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Anki</title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="icon" type="image/png" href="favicon.png">
+</head>
+<body>
+    <div>hello bitch</div>
+</body>
+</html>
+
+/Applications/XAMPP/xamppfiles/htdocs/AnkiProject/index.php
