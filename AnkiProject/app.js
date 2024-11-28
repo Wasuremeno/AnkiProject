9e20419836035b0ac601a724b7062747560fee68
Wasuremeno <wasuremono19@gmail.com>
2024-11-28 21:34:08 +0300

    i've added learning, review and mixed mod to this project

---
+++ AnkiProject/app.js
@@ -0,0 +1,6 @@
+function familiar(){
+    document.getElementById("green-button").classList.toggle("affirmative");
+}
+function unfamiliar(){
+    document.getElementById("grey-button").classList.toggle("negative");
+}
--- AnkiProject/index.php
+++ AnkiProject/index.php
@@ -8,16 +8,17 @@
     <link rel="icon" type="image/png" href="favicon.png">
 </head>
 <body>
+    <nav></nav>
     <div class="container">
     <div class="category-section">
         <div class="option">
-            <span>hello bitch</span>
+            <a href="/AnkiProject/learnign.php">Learn new words</a>
         </div>
         <div class="option">
-            <span>hello bitch</span>
+            <a href="/AnkiProject/review.php ">Review words</a>
         </div>
         <div class="option">
-            <span>hello bitch</span>
+            <a href="/AnkiProject/mixedMod.php">Mixed mod</a>
         </div>
         
     </div>
